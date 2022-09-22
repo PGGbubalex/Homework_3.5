dc9f35e3a9e56a2ee0a55f20b2b566a1d3512e53
Igor <99yearago99@gmail.com>
2022-09-22 20:25:46 +0300

    Task 5

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int totalCansOfPaint = 120;
+        int cansOfWhitePaintForOneClass = 2;
+        int cansOfBrownPaintForOneClass = 4;
+
+        int totalNumberOfClasses = totalCansOfPaint / (cansOfBrownPaintForOneClass + cansOfWhitePaintForOneClass);
+        int totalNumberOfCansOfWhitePaint = totalNumberOfClasses * cansOfWhitePaintForOneClass;
+        int totalNumberOfCansOfBrownPaint = totalNumberOfClasses * cansOfBrownPaintForOneClass;
+
+            System.out.println("В школе, где " + totalNumberOfClasses + " классов, нужно " + totalNumberOfCansOfWhitePaint + " белой краски и " + totalNumberOfCansOfBrownPaint + " банок коричневой краски");
+    }
+}
